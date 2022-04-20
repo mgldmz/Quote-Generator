e165073728f77645fa3eed102c63b8e7a9a317df
Miguel Domínguez <migueldominguezguevara@gmail.com>
2022-04-20 14:57:21 -0500

    added buttons functionality

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
+
+
 let apiQuotes = [];
 
 
@@ -6,7 +15,21 @@ let apiQuotes = [];
 function newQuote(){
     // Pick a random   quote
     const quote= apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    //Check if author field is blank and replace with 'unknown'
+    if(!quote.author){
+        authorText.textContent = 'Unknown';
+    }else{
+        authorText.textContent = quote.author;
+    }
+    //Check quote lenght to determine the styling
+    if(quote.text.length > 120){
+        quoteText.classList.add('long-quote');
+    }else{
+        quoteText.classList.remove('long-quote');
+
+    }
+
+    quoteText.textContent = quote.text;
 }
 
 
@@ -29,6 +52,18 @@ async function getQuotes(){
 
     }
 }
+
+//Tweet Quote
+
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event Listeners
+
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 //On Load
 getQuotes();
