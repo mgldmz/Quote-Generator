f144cbb7330268d298f37aaa9f95171d72a8dacf
Miguel Domínguez <migueldominguezguevara@gmail.com>
2022-04-20 15:14:16 -0500

    Added loading screen

--- script.js
+++ script.js
@@ -3,16 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
-
-
-
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+//Show loading
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden= true;
+}
 
+//Hide Loading
+function complete(){
+    loader.hidden = true;
+    quoteContainer.hidden= false;
+}
 
 // Show new quote
 function newQuote(){
+    loading();
     // Pick a random   quote
     const quote= apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     //Check if author field is blank and replace with 'unknown'
@@ -30,6 +39,7 @@ function newQuote(){
     }
 
     quoteText.textContent = quote.text;
+    complete();
 }
 
 
@@ -41,6 +51,7 @@ https://quotes-react.netlify.app/
 https://type.fit/api/quotes*/
 
 async function getQuotes(){
+    loading();
     const apiUrl='https://type.fit/api/quotes';
     try{
         const response = await fetch(apiUrl);
